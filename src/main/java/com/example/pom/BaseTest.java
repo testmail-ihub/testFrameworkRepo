cba3f691407ac825d53a49414dc79df5985359fa
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-10-08 12:54:14 +0530

    Add BaseTest.java for Selenium Page Object Model structure

---
+++ src/main/java/com/example/pom/BaseTest.java
@@ -0,0 +1,35 @@
+package com.example.pom;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.time.Duration;
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        String browser = System.getProperty("browser", "chrome");
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            throw new UnsupportedOperationException("Unsupported browser: " + browser);
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
