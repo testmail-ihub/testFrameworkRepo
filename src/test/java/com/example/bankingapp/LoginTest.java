31ff70e9383218a14a4b5f80a6988dc1d8dcbef3
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-10-08 10:03:19 +0530

    Update LoginTest.java to read credentials from creds.properties

--- src/test/java/com/example/bankingapp/LoginTest.java
+++ src/test/java/com/example/bankingapp/LoginTest.java
@@ -4,17 +4,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-import java.time.Duration;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import static org.testng.Assert.assertTrue;
+import java.time.Duration;
 public class LoginTest extends BaseTest {
     @Test
     public void testSuccessfulLogin() {
         WebDriver driver = getDriver();
         LoginPage loginPage = new LoginPage(driver);
         DashboardPage dashboardPage = new DashboardPage(driver);
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream("src/test/resources/creds.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load credentials from properties file", e);
+        }
         loginPage.navigate();
-        loginPage.inputUsername("validUsername");
-        loginPage.inputPassword("validPassword");
+        loginPage.inputUsername(props.getProperty("username"));
+        loginPage.inputPassword(props.getProperty("password"));
         loginPage.submit();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.urlContains("dashboard"));
