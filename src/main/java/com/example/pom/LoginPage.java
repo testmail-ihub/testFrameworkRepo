9fba49deb310a317177aebb12c8749e71e8c6abd
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-10-08 12:52:39 +0530

    Add LoginPage.java for Selenium Page Object Model structure

---
+++ src/main/java/com/example/pom/LoginPage.java
@@ -0,0 +1,29 @@
+package com.example.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "username")
+    private WebElement usernameInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password) {
+        enterText(usernameInput, username);
+        enterText(passwordInput, password);
+        clickElement(loginButton);
+    }
+}
