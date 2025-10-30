9c9f22aad0fcd6adb9fa56ed0a84370a5f221f31
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-10-30 14:08:56 +0530

    Add Python code to add two natural numbers

---
+++ add_numbers.py
@@ -0,0 +1,8 @@
+def add_two_numbers(num1, num2):
+    return num1 + num2
+
+# Example usage:
+number1 = 5
+number2 = 7
+sum_result = add_two_numbers(number1, number2)
+print(f"The sum of {number1} and {number2} is: {sum_result}")
