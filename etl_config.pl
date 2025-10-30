e0808ac1056c5ebdbcfe9ef7d719f9633b40a2eb
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-10-30 13:48:38 +0530

    Create etl_config.pl for ETL pipeline configuration

---
+++ etl_config.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# Database configuration
+our $DB_HOST = 'localhost';
+our $DB_NAME = 'source_db';
+our $DB_USER = 'your_username';
+our $DB_PASS = 'your_password';
+
+# File paths
+our $RAW_DATA_CSV = 'raw_data.csv';
+our $CLEANED_DATA_CSV = 'cleaned_data.csv';
+
+1;
